4b72c4bf6755297fdb2965d6b7b13d219383009f
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2022-07-24 17:19:58 +0530

    Created The Inventory System

--- Assets/Scripts/Character/Pickups.cs
+++ Assets/Scripts/Character/Pickups.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Pickups : MonoBehaviour
 {
@@ -17,13 +19,14 @@ public class Pickups : MonoBehaviour
     public int hamCount=0;
     public int ironCount=0;
     public int jewelCount=0;
-
+    [SerializeField] GameObject warningText;
     CharacterController character;
 
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
+        warningText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -59,37 +62,72 @@ public class Pickups : MonoBehaviour
 
         if(other.CompareTag("Iron"))
         {
-            other.GetComponent<BoxCollider>().enabled=false;
-            if(!Inventory.hasIron)
+            if(Inventory.itemsCount < 100)
             {
-                Inventory.hasIron = true;
+                other.GetComponent<BoxCollider>().enabled=false;
+                if(!Inventory.hasIron)
+                {
+                    Inventory.hasIron = true;
+                }
+                ++ironCount;
+                ++Inventory.itemsCount;
+                Destroy(other.gameObject,0.1f);
             }
-            ++ironCount;
-            Destroy(other.gameObject,0.1f);
+            else
+            {
+                warningText.SetActive(true);
+                warningText.GetComponent<Text>().text= "Inventory is full Cannot Add Items.";
+                Invoke("disableWarningText",2f);
+            }
         }	
 
         if(other.CompareTag("Ham"))
         {
-            other.GetComponent<CapsuleCollider>().enabled=false;
-            if(!Inventory.hasHam)
+            if(Inventory.itemsCount < 100)
             {
-                Inventory.hasHam = true; 
+                other.GetComponent<CapsuleCollider>().enabled=false;
+                if(!Inventory.hasHam)
+                {
+                    Inventory.hasHam = true; 
+                }
+                ++hamCount;
+                ++Inventory.itemsCount;
+                Destroy(other.gameObject,0.1f);
             }
-            ++hamCount;
-            Destroy(other.gameObject,0.1f);
+            else
+            {
+                warningText.SetActive(true);
+                warningText.GetComponent<TextMeshProUGUI>().text= "Inventory is full Cannot Add Items.";
+                Invoke("disableWarningText",2f);
+            }
         }
 
         if(other.CompareTag("Jewel"))
         {
-            other.GetComponent<CapsuleCollider>().enabled=false;
-            if(!Inventory.hasJewel)
+            if(Inventory.itemsCount < 100)
             {
-            Inventory.hasJewel = true;
+                other.GetComponent<CapsuleCollider>().enabled=false;
+                if(!Inventory.hasJewel)
+                {
+                Inventory.hasJewel = true;
+                }
+                ++jewelCount;
+                ++Inventory.itemsCount;
+                Destroy(other.gameObject,0.1f);
             }
-            ++jewelCount;
-            Destroy(other.gameObject,0.1f);
+            else
+            {
+                warningText.SetActive(true);
+                warningText.GetComponent<Text>().text= "Inventory is full Cannot Add Items.";
+                Invoke("disableWarningText",2f);
+            }
         }
 	}
+
+    void disableWarningText()
+    {
+        warningText.SetActive(false);
+    }
 
 
 
--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Inventory : MonoBehaviour
 {
@@ -14,7 +15,14 @@ public class Inventory : MonoBehaviour
     public static bool hasIron;
     public static bool hasHam;
     public static bool hasJewel;
-
+    [SerializeField] GameObject warningText;
+    [SerializeField] Transform plyaerTrans;
+    [SerializeField] GameObject hamPrefab;
+    [SerializeField] GameObject ironPrefab;
+    [SerializeField] GameObject jewelPrefab;
+    [SerializeField] GameObject handHam;
+    [SerializeField] GameObject handIron;
+    [SerializeField] GameObject handJewel;   
     [SerializeField] Image hamImage;
     [SerializeField] Text hamText;
     [SerializeField] Text hamCountText;
@@ -29,6 +37,24 @@ public class Inventory : MonoBehaviour
     [SerializeField] Sprite hamSprite;
     [SerializeField] Sprite ironSprite;
     [SerializeField] Sprite jewelSprite;
+
+    [SerializeField] Image desImage;
+    [SerializeField] TextMeshProUGUI desText;
+ 
+
+    [SerializeField] TextMeshProUGUI inventoryCountText;
+
+    Button hamBtton;
+    Button ironButton;
+    Button jewelButton;
+
+    bool hasSelectedHam;
+    bool hasSelectedIron;
+    bool hasSelectedJewel;
+
+    bool holdsHam;
+    bool holdsIron;
+    bool holdsJewel;
 
 
 
@@ -46,15 +72,34 @@ public class Inventory : MonoBehaviour
         hamCountText.enabled = false;
         ironCountText.enabled = false;
         jewelCountText.enabled = false;
+
+        hamBtton = hamImage.GetComponent<Button>();
+        ironButton = ironImage.GetComponent<Button>();
+        jewelButton = jewelImage.GetComponent<Button>();
+
+        hamBtton.enabled  = false;
+        ironButton.enabled  = false;
+        jewelButton.enabled  = false;
+
+        handHam.SetActive(false);
+        handIron.SetActive(false);
+        handJewel.SetActive(false);
+
+        holdsHam = false;
+        holdsIron = false;
+        holdsJewel = false;
+
+        warningText.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        checkCount();
         checkHam();
         checkIron();
         checkJewel();
+        inventoryCountText.text =  "Inventory (" + itemsCount + " / " + 100 + ")";
     }
 
     public void loadInventory()
@@ -73,55 +118,195 @@ public class Inventory : MonoBehaviour
         }
     }
 
-    void checkCount()
-    {
-
-    }
+    
     void checkHam()
     {
+        if(pickups.hamCount <=0)
+        {
+            hasHam = false;
+        }
         if(hasHam)
         {
             hamImage.sprite = hamSprite;
             hamText.enabled = true;
             hamCountText.enabled=true;
             hamCountText.text = pickups.hamCount.ToString();
+            hamBtton.enabled = true;
         }
 
         else
         {
-
+            hamImage.sprite = empty;
+            hamText.enabled = false;
+            hamCountText.enabled=false;
+            hamBtton.enabled = false;
         }
     }
 
     void checkIron()
     {
+        if(pickups.ironCount <=0)
+        {
+            hasIron = false;
+        }
+
         if(hasIron)
         {
             ironImage.sprite = ironSprite;
             ironText.enabled = true;
             ironCountText.enabled=true;
             ironCountText.text = pickups.ironCount.ToString();
+            ironButton.enabled = true;
         }
 
         else
         {
-
+            ironImage.sprite = empty;
+            ironText.enabled = false;
+            ironCountText.enabled=false;
+            ironButton.enabled = false;
         }
     }
 
     void checkJewel()
     {
+        if(pickups.jewelCount <=0)
+        {
+            hasJewel = false;
+        }
+
         if(hasJewel)
         {
             jewelImage.sprite = jewelSprite;
             jewelText.enabled = true;
             jewelCountText.enabled=true;
             jewelCountText.text = pickups.jewelCount.ToString();
+            jewelButton.enabled = true;
         }
 
         else
         {
-
+            jewelImage.sprite = empty;
+            jewelText.enabled = false;
+            jewelCountText.enabled=false;
+            jewelButton.enabled = false;
         }
+    }
+
+    public void loadHamDes()
+    {
+        descriptionPanel.SetActive(true);
+        desImage.sprite = hamSprite;
+        desText.text = "Ham \n\n A Leg of Ham is very Tasty Snack but Also Can be Used as a weapon";
+        hasSelectedHam = true; 
+        hasSelectedIron = false;
+        hasSelectedJewel = false;
+    }
+
+    public void loadIronDes()
+    {
+        descriptionPanel.SetActive(true);
+        desImage.sprite = ironSprite;
+        desText.text = "Iron \n\n A Iron piece Can be Used to Build Weapons";
+        hasSelectedHam = false; 
+        hasSelectedIron = true;
+        hasSelectedJewel = false;
+    }
+
+    public void loadJewelDes()
+    {
+        descriptionPanel.SetActive(true);
+        desImage.sprite = jewelSprite;
+        desText.text = "Jewel \n\n A Jewel piece can be Used sharpen Waepons";
+        hasSelectedHam = false; 
+        hasSelectedIron = false;
+        hasSelectedJewel = true;
+    }
+
+    public void Equip()
+    {
+        if(hasSelectedHam)
+        {
+            handHam.SetActive(true);
+            handIron.SetActive(false);
+            handJewel.SetActive(false);
+            holdsHam = true;
+            holdsIron = false;
+            holdsJewel = false;
+        }
+
+        if(hasSelectedIron)
+        {
+            handHam.SetActive(false);
+            handIron.SetActive(true);
+            handJewel.SetActive(false);
+            holdsHam = false;
+            holdsIron = true;
+            holdsJewel = false;
+        }
+
+        if(hasSelectedJewel)
+        {
+            handHam.SetActive(false);
+            handIron.SetActive(false);
+            handJewel.SetActive(true);
+            holdsHam = false;
+            holdsIron = false;
+            holdsJewel = true;
+        }
+    }
+
+    public void Drop()
+    {
+        if(!holdsHam && !holdsIron && !holdsJewel)
+        {
+            warningText.SetActive(true);
+            warningText.GetComponent<TextMeshProUGUI>().text = "Hold Something To Drop";
+            Invoke("disableWarningText",2f);
+        }
+
+        if(holdsHam)
+        {
+            Instantiate(hamPrefab, plyaerTrans.position + new Vector3(2f,0,2f), plyaerTrans.rotation);
+            --pickups.hamCount;
+            holdsHam = false;
+            handHam.SetActive(false);
+            --itemsCount;
+            if(pickups.hamCount<=0)
+            {
+                hasHam = false;
+            }
+        }
+
+        else if(holdsIron)
+        {
+            Instantiate(ironPrefab, plyaerTrans.position + new Vector3(2f,0,2f), plyaerTrans.rotation);
+            --pickups.ironCount;
+            holdsIron = false;
+            handIron.SetActive(false);
+            --itemsCount;
+            if(pickups.ironCount<=0)
+            {
+                hasIron = false;
+            }
+        }
+
+        else if(holdsJewel)
+        {
+            Instantiate(jewelPrefab, plyaerTrans.position + new Vector3(2f,0,2f), plyaerTrans.rotation);
+            --pickups.jewelCount;
+            holdsJewel = false;
+            handJewel.SetActive(false);
+            --itemsCount;
+            if(pickups.jewelCount<=0)
+            {
+                hasJewel = false;
+            }
+        }
+    }
+
+    void disableWarningText()
+    {
+        warningText.SetActive(false);
     }
 }
